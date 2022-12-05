622039b452f7fc5350c8e8ae11dd3e5a27dd5dfd
Marco-Sorrentino <sorrentmarco@gmail.com>
2022-12-05 17:05:23 +0100

    fixed

--- index.js
+++ index.js
@@ -75,5 +75,4 @@ const name2 = "John";
 
 console.log(name1 === name2);
 
-console.log(name1.toLowerCase());
-console.log(name2.toLowerCase());
+console.log(name1.toLowerCase() === name2.toLowerCase());
